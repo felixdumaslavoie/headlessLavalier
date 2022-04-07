32e550182629de1c4f0228e488a154074d53a346
felixdumaslavoie <felixdumaslavoie@gmail.com>
2022-04-07 16:45:08 -0400

    Test du programme

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-fn main() {
-    println!("Hello, world!");
-}
+use psimple::Simple;
+use pulse::stream::Direction;
+use pulse::sample::{Spec, Format};
 
+let spec = Spec {
+    format: Format::S16NE,
+    channels: 2,
+    rate: 48000,
+};
+assert!(spec.is_valid());
 
-
-
-
-
-
-
-
+let s = Simple::new(
+    None,                // Use the default server
+    "FooApp",            // Our application’s name
+    Direction::Playback, // We want a playback stream
+    None,                // Use the default device
+    "Music",             // Description of our stream
+    &spec,               // Our sample format
+    None,                // Use default channel map
+    None                 // Use default buffering attributes
+).unwrap();
 
 
 
